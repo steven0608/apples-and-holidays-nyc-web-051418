bac23b75a7a60d223cd906e0506cf7511ceb08b8
Steven Chen <ysteven.chen2@gmail.com>
2018-05-07 17:46:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   hash={}
   hash << :holiday_name
   hash[:holiday_name] << supply_array
-  
+
 holiday_hash[:season] << hash
 holiday_hash
 end
